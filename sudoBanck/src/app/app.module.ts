19c342df7fb42f4d7382707935e2efaa4a3dbe25
Matheus <matheus.lira93@gmail.com>
2021-05-24 23:20:41 -0300

    preparando ambiente

--- sudoBanck/src/app/app.module.ts
+++ sudoBanck/src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BodyComponent } from './app/site/body/body.component';
+import { HeaderComponent } from './site/header/header.component';
+import { FooterComponent } from './site/footer/footer.component';
+import { ToolsComponent } from './tools/tools.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BodyComponent,
+    HeaderComponent,
+    FooterComponent,
+    ToolsComponent
   ],
   imports: [
     BrowserModule,
