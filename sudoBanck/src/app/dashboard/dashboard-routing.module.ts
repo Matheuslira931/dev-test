bba23efae030720bffd7c526606c0d6d7e480d9c
Matheus <matheus.lira93@gmail.com>
2021-05-31 10:22:03 -0300

    ajustes de tela/menu, home

--- sudoBanck/src/app/dashboard/dashboard-routing.module.ts
+++ sudoBanck/src/app/dashboard/dashboard-routing.module.ts
@@ -1,11 +1,12 @@
 import { ExtratoComponent } from './extrato/extrato.component';
 import { Routes } from '@angular/router';
 import { DashboardComponent } from './dashboard.component';
+import { HomeComponent } from './home/home.component';
 
 export const DashboardRouting: Routes = [
   {
     path: '',
     component: DashboardComponent,
-    children: [{path: 'extrato', component: ExtratoComponent}]
+    children: [{path: 'home', component: HomeComponent}, {path: 'extrato', component: ExtratoComponent}]
   }
 ];
